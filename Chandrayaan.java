2f939c5485f146bb23ac03e3d4489fcae4e15b39
SoubhagyaRanjanPanda <142379205+SoubhagyaRanjanPanda@users.noreply.github.com>
2023-08-16 13:46:24 +0530

    Update Chandrayaan.java

--- Chandrayaan.java
+++ Chandrayaan.java
@@ -2,35 +2,37 @@ public class Chandrayaan {
 
     public String processCharArray(char[] charArray) {
         StringBuilder result = new StringBuilder();
-        int x, y , z=0;
-        char Direction;
+        int i, x, y , z=0;
+        String[] Direction = new String[4];
+        String[] Direction = {"N", "E", "S" ,"W"};
+        char D;
         for (char ch : charArray) {
             if (ch == 'f') {
                 y++;
-                Direction='N';
-                return (x,y,z,Direction);
+                D=Direction[i];
+                return (x,y,z,D);
             }
             if (ch=='r'){
-                Direction='E';
-                return (x,y,z,Direction);
+                D=Direction[i++];
+                return (x,y,z,D);
             }
             if(ch=='u'){
-                Direction='U';
-                return (x,y,z,Direction);
+                D='U';
+                return (x,y,z,D);
             }
             if(ch='b'){
-                if(Direction='d'){
+                if(D='U'){
                     z--;
-                    return (x,y,z,Direction);
+                    return (x,y,z,D);
                 }
 
-                else if (Direction='u'){
+                else if (D='d'){
                     z++;
-                    return (x,y,z,Direction);
+                    return (x,y,z,D);
                 }
                 }
             if(ch='l'){
-                Direction='W';
+                D=Direction[i--];
                 return (x,y,z,Direction);
             }
             }
